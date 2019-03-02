000e6a7cd009cf096086c918fe0057ab67386595
unknown <srodriki@gmail.com>
2019-03-02 12:17:07 -0300

    Added basic handling for search Added code to call a server-side method.

--- force-app/main/default/aura/ObjectAutocomplete/ObjectAutocompleteController.js
+++ force-app/main/default/aura/ObjectAutocomplete/ObjectAutocompleteController.js
@@ -8,5 +8,6 @@
     loadOptions: function(component, event, helper) {
         // TODO: load items from the database, and then store them inside the items attr.
         // TODO: load items only if the minimum amount of characters has been entered.
+        helper.loadOptions(component, event.target.value);
     }
 })
--- force-app/main/default/aura/ObjectAutocomplete/ObjectAutocompleteHelper.js
+++ force-app/main/default/aura/ObjectAutocomplete/ObjectAutocompleteHelper.js
@@ -6,5 +6,38 @@
     hideOptions: function(component) {
         var optionsContainer = component.find('lookup');
         $A.util.removeClass(optionsContainer, 'slds-is-open');
+    },
+    loadOptions: function(component, searchCriteria) {
+        if (searchCriteria.length >= 4) {
+            this.searchResults(component, searchCriteria);
+        }
+    },
+    searchResults: function(component, searchCriteria) {
+        // get basic attributes
+        var searchFields = component.get('v.searchFields');
+        var limit = component.get('v.limit');
+        var retrieveFields = component.get('v.fields');
+        var sObjectType = component.get('v.sObjectType');
+
+        // link to the server-side action from the component
+        var action = component.get('c.searchResults');
+
+        // set all parameters for the action
+        action.setParams({
+            sObjectType,
+            searchFields,
+            limit,
+            retrieveFields
+        });
+        let helper = this;
+        //prepare a callback
+        action.setCallback(this, (response) => {
+            helper.onSearchResultsCompleted(component, response);
+        });
+        // enqueue for execution.
+        $A.enqueueAction(action);
+    },
+    onSearchResultsCompleted: function(component, response) {
+        console.log(JSON.stringify(response));
     }
 })
