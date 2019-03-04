f4db160374257781c1d82923363c59935759ffd1
unknown <srodriki@gmail.com>
2019-03-04 16:28:22 -0300

    Fixes last commit by adding missing changes

--- force-app/main/default/aura/WeightMeasureInput/WeightMeasureInputHelper.js
+++ force-app/main/default/aura/WeightMeasureInput/WeightMeasureInputHelper.js
@@ -1,5 +1,19 @@
 ({
-    helperMethod : function() {
-
+    saveMeasure : function(component) {
+        // TODO: 
+        // 1. get action from controller
+        // 2. set params and callback
+        // 3. enqueue action.
+    },
+    resetChanges: function(component) {
+        component.set('v.record', { sObjectType: 'Weight_Measure__c'});
+    }, 
+    onSaveMeasureComplete: function(component, measure) {
+        // TODO:
+        // 1. emit save
+        // 2. reset changes
+    },
+    emitSaveMeasure: function(component, measure) {
+        
     }
 })
